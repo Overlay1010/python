601c2175229f3c56954e8122789ad4da34595173
Overlay1010 <overlay1010@gmail.com>
2026-07-28 11:11:27 +0900

    [level 0] Title: [PCCE 기출문제] 4번 / 병과분류, Time: 13.00 ms, Memory: 9.05 MB -BaekjoonHub

--- Python3/프로그래머스/0/340204. ［PCCE 기출문제］ 4번 ／ 병과분류/［PCCE 기출문제］ 4번 ／ 병과분류.py
+++ Python3/프로그래머스/0/340204. ［PCCE 기출문제］ 4번 ／ 병과분류/［PCCE 기출문제］ 4번 ／ 병과분류.py
@@ -3,11 +3,11 @@ last_four_words = code[-4:]
 
 if last_four_words == "_eye":
     print("Ophthalmologyc")
-elif last_four_words =='head':
+elif last_four_words =="head":
     print("Neurosurgery")
-elif last_four_words == 'infl':
+elif last_four_words =="infl":
     print("Orthopedics")
-elif last_four_words =='skin':
+elif last_four_words == "skin":
     print("Dermatology")
 else:
     print("direct recommendation")
